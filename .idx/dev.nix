79ed78787918138a22acca5814965c46028c8ed1
neurocoderz <neurocoderz@gmail.com>
2025-05-29 13:49:20 +0000

    Feat: Configure .idx/dev.nix for Python, Node, Firebase CLI, TS, Protobuf and install project dependencies

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,11 +2,16 @@
 { pkgs, ... }: {
   channel = "stable-24.05"; 
   packages = [
-    pkgs.git # Только Git
+    pkgs.git
+    pkgs.python312
+    pkgs.nodejs_20
+    pkgs.nodePackages.typescript 
+    pkgs.nodePackages.typescript-language-server
+    pkgs.protobuf
   ];
   idx = {
-    extensions = []; # Пока без расширений VS Code
-    previews = { enable = false; }; # Пока без превью
+    extensions = ["ms-python.python" "ms-vscode.vscode-typescript-next" "zxh404.vscode-proto3"]; 
+    previews = { enable = false; }; 
     workspace = {
       onCreate = {};
       onStart = {};
